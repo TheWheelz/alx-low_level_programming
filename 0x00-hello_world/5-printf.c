a27b3ae5c87ee51d1c84a90326cc75aa4fc8992d
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-12 12:17:54 -0800

    This is a commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void);
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
