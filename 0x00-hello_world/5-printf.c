a0a8f90867a3cec37795f08db02d32b146d9354c
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-12 00:37:12 -0800

    This prints out a message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include < stdio.h >
+
+/**
+ * main - Entry Point
+ *
+ * Description: prints with proper grammar,
+ * but the outcome is a piece of art.
+ *
+ * Returns 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0)
+}
