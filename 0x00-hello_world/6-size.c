7ede30ceac2b6b6e7164f6d68ec0ac03929dccf4
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-12 12:32:15 -0800

    This prints the variable types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  *main - prints the size of various types
  *Return: 0 if exited properly, non-zero otherwise
  */
@@ -18,7 +17,7 @@ int main(void)
 
 		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));	
+		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
 		printf("Size of a float: %d byte(s)\n", sizeof(float));
 
