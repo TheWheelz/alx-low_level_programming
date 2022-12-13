ff97ce7e36f97f0fe69b872c210fae71d8e0d509
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-12 19:11:22 -0800

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,6 +13,8 @@ int main(void)
 
 		for (i = 0; i < 100; i++)
 		{
+			for (j = 0; j < 100; j++)
+			{
 			if (i < j)
 			{
 			putchar((i / 10) + 48);
@@ -24,6 +26,7 @@ int main(void)
 			{
 				putchar(',');
 				putchar(' ');
+			}
 			}
 			}
 		}
