260d7fa4d88a4c697e07c43d437595e1711b3dca
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-22 13:24:09 -0800

    This prints a buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
