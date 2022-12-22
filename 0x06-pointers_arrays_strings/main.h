18cbb2030603c691bbbeef1c12f18bbc42109ac7
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-22 13:21:45 -0800

    infinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
