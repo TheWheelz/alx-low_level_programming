a8732e6491853831c2c8c9a6f0ffc5fab0ae59a7
Oluwafemi Busari <busarioluwafemi@gmail.com>
2022-12-22 03:23:15 -0800

    This concatennates two strings together

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,34 @@
 #include "main.h"
 
 /**
- * _strcat - concatennates two strings
- * @dest: copy to
- * @src: copy from
+ * _strcat - function that concatenates two strings.
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
  *
- * Return: pointer to dest
+ * Return: pointer to resulting string @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
 
-	i = 0;
-	while (dest[i] i != '0\')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	int c, c2;
+
+	c = 0;
+
+	/*find the size of dest array*/
+
+	while (dest[c])
+
+		c++;
+
+	/* iterate through each src array value without the null byte*/
+
+	for (c2 = 0; src[c2] ; c2++)
+
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+
+		dest[c++] = src[c2];
+
 	return (dest);
+
 }
