254523f8dceb4340e6febfee9a35c2b263271094
Oluwafemi Busari <busarioluwafemi@gmail.com>
2023-01-06 14:32:26 -0800

    Multiplies two num

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 
 /**
@@ -12,14 +11,9 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-
 	printf("Error\n");
-
 	return (1);
 	}
-
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 	return (0);
-
 }
